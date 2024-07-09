d45d9fe86050d9ad6e61b03db79c39e63a74be14
Unique <mujeem000@gmail.com>
2024-07-09 11:20:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7352893042:AAHDhltLe6d1DLa3Jxetito-r50Rh7UqZpU")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7327949614:AAF09QsqhGNMArXg2Ri1ZytJOb6UEfQY-Mk")
     API_ID = int(os.environ.get("API_ID", "21179966"))
     API_HASH = os.environ.get("API_HASH", "d97919fb0a3c725e8bb2a25bbb37d57c")
     AUTH_USERS = os.environ.get("AUTH_USERS", "7326397503")
